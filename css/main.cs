8dc861483ab5f3fd63c39408e7c7ec24f8fbd6ed
monicaMR <monicamrdam@gmail.com>
2023-04-27 05:46:49 +0200

    diseño  index.html terminado

--- css/main.cs
+++ css/main.cs
@@ -1,3 +1,15 @@
+/* Verde #367309 */
+
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+
 .titulo{
     text-align: center;
     font-family: Arial Black;
@@ -5,21 +17,72 @@
     font-size: 50px;
     color: white;
     text-shadow: 0 1px 0 #ddd, 0 2px 0 #ccc, 0 3px 0 #bbb, 0 4px 0 #aaa, 0 5px 0 #acacac, 0 6px 1px rgba(0,0,0,0.1), 0 0 5px rgba(0,0,0,0.1), 0 1px 3px rgba(0,0,0,0.3), 0 3px 5px rgba(0,0,0,0.2), 0 5px 10px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.2), 0 20px 20px rgba(0,0,0,0.15);
+    padding: 20px;
 }
 
 nav {
    	margin: 0;
     padding: 0;
     overflow: hidden;
-    background-color: green;
+    background-color: #367309;
     text-align: center;
 }
 
 
 a{
+    text-decoration:none;
     color: white;
+    font-weight:600;
     font-size: 20px;
     text-align: center;
     padding: 14px 16px;
     text-decoration: none;
 }
+
+section{
+     text-align: center;
+     padding: 20px;
+}
+
+
+.boton_registrarse{
+    text-decoration:none;
+    font-weight:600;
+    font-size:20px;
+    color:white;
+    padding-top:10px;
+    padding-bottom:10px;
+    padding-left:40px;
+    padding-right:40px;
+    background-color:#367309;
+}
+
+
+
+footer {
+  text-align: center;
+  padding: 3px;
+  background-color: #367309;
+  color: white;
+}
+
+.footer .row{
+    width:100%;
+    color:gray;
+    font-size:0.8em;
+
+}
+
+
+.footer .row a{
+    text-decoration:none;
+    transition:0.5s;
+           font-size:15px;
+
+}
+
+
+.footer .row a:hover{
+    color:#fff;
+}
+
